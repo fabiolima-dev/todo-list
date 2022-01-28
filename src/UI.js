8915af099824d5cada950cbec50795d8ff377e8e
Fabio <fabiolima@fabiolima.dev>
2022-01-28 07:01:57 -0300

    first screen started

--- src/UI.js
+++ src/UI.js
@@ -1,29 +1,75 @@
+import './style.css';
+
 const body = document.querySelector("body");
 
 const projectsContainer = (() => {
     const render = () => {
-        body.appendChild(projectsContainer);
+        body.appendChild(container);
     };
     const remove = () => {
-
+        body.removeChild(container);
     };
-    const projectsContainer = document.createElement("aside");
-    projectsContainer.id = "projects-container";
+    const addProjectInput = () => {
+        const input = document.createElement("input");
+        input.setAttribute("type", "text");
+
+        console.log("teste");
+    }
+    const addProject = () => {
+        const project = document.createElement("h3");
+        project.classList.add("white");
+
+    }
+    const container = document.createElement("aside");
+    container.id = "projects-container";
 
     const title = document.createElement("h2");
     title.textContent = "Todo List"
     title.classList.add("white");
+    title.id = "projects-container-title";
 
-    const addProject = document.createElement("h3");
-    addProject.textContent = "+ Add Project";
-    addProject.classList.add("white");
+    const addProjectElement = document.createElement("h3");
+    addProjectElement.textContent = "+ Add Project";
+    addProjectElement.classList.add("white");
+    addProjectElement.addEventListener("click", () => {
+        addProjectInput();
+    });
 
-    projectsContainer.append(
+    container.append(
         title,
-        addProject
+        addProjectElement
     )
 
     return {
-        render
+        render,
+        remove
     };
-});
+})();
+
+const startScreen = (() => {
+    const render = () => {
+        body.appendChild(container);
+    }
+    const remove = () => {
+        body.removeChild(container);
+    }
+    const container = document.createElement("main");
+    container.id = "start-screen-container"
+
+    const title = document.createElement("h1");
+    title.textContent = "Create a Project to Start"
+
+    container.appendChild(title);
+
+    return {
+        render,
+        remove
+    }
+})();
+
+const loadPage = () => {
+    projectsContainer.render();
+    startScreen.render();
+}
+
+export {loadPage};
--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,21 @@ module.exports = {
   },
   devServer: {
     static: './dist',
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,    
+        type: 'asset/resource',    
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,    
+        type: 'asset/resource',    
+      },
+    ],
+  },
 };
